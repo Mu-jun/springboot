383ede37f03018701f4e7b915d639e703c92a480
Mu-jun <suo24691@gmail.com>
2023-02-03 19:47:15 +0900

    독립 실행형 스프링 애플리케이션 - 스프링 컨테이너 통합
    스프링 컨테이너의 초기화 작업 중에 호출되는 훅 메소드(onRefresh)에 서블릿 컨테이너(톰캣)을 초기화하고 띄우는 코드를 넣는다.

--- src/main/java/mjspring/springboot/SpringbootApplication.java
+++ src/main/java/mjspring/springboot/SpringbootApplication.java
@@ -9,18 +9,25 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this))
+						.addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class); // Bean 등록
 		applicationContext.registerBean(SimpleHelloService.class); // 생성 순서와 DI는 스프링이 알아서 구현체 정보를 가지고 결정함.
 		applicationContext.refresh(); // 초기화(재구성?)
 
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontcontroller",
-					new DispatcherServlet(applicationContext)
-				).addMapping("/*");
-		});
-		webServer.start();
+
 	}
 
 }
