edcdeaad48fee3053c8fec8400b89d9f9c3c9ffe
Mu-jun <suo24691@gmail.com>
2023-02-03 16:58:25 +0900

    Hello 컨트롤러
    HTTP 요청 응답 확인

---
+++ src/main/java/mjspring/springboot/HelloController.java
@@ -0,0 +1,12 @@
+package mjspring.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello(String name) {
+        return "Hello " + name;
+    }
+}
