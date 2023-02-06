fe7c38f13151d0aa752ae2a46da6989a72e62ee5
Mu-jun <suo24691@gmail.com>
2023-02-06 09:44:46 +0900

    테스트 코드를 이용한 테스트
    TestRestTemplate를 이용해 실행 중인 WAS에 Http요청을 보내고 응답을 검증한다.

---
+++ src/test/java/mjspring/springboot/HelloApiTest.java
@@ -0,0 +1,26 @@
+package mjspring.springboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+        ResponseEntity<String> resp = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // 응답검증 3가지
+        // status code 200
+        Assertions.assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // header(content-type) text/plain
+        Assertions.assertThat(resp.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        // body Hello Spring
+        Assertions.assertThat(resp.getBody()).isEqualTo("Hello Spring");
+    }
+}
