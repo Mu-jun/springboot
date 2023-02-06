3b9c3103b6ed7bac61be673647e48e6a48ba85f8
Mu-jun <suo24691@gmail.com>
2023-02-06 14:38:52 +0900

    메타 애노테이션과 합성 애노테이션
    메타 애노테이션
    - 애노테이션1 <- 애노테이션2 <- 애노테이션3
    합성 애노테이션
    - 애노테이션1,애노테이션2 <- 애노테이션3

---
+++ src/main/java/mjspring/springboot/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package mjspring.springboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) // class, interface, enum
+@Configuration
+@ComponentScan
+public @interface MySpringBootApplication {
+}
