32f02c616cac23538d47e6c3547127d1e742ddd1
Mu-jun <suo24691@gmail.com>
2023-02-06 18:52:40 +0900

    자동 구성 정보 대체하기
    서블릿 웹 서버 팩토리에 해당하는 Bean을 직접 정의
    사용자 구성 정보가 자동 구성 정보보다 먼저 적용된다.

--- src/main/java/mjspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/mjspring/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package mjspring.config.autoconfig;
 
 import mjspring.config.ConditionalMyOnClass;
 import mjspring.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory") // Default는 메소드 이름이 Bean의 이름이 된다.
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
