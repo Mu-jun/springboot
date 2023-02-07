4fd74305a8aab62532b62b650819185d22156af6
Mu-jun <suo24691@gmail.com>
2023-02-07 17:43:25 +0900

    Hello 리포지토리
    - countOf로 찾을 때 데이터가 없으면 0, 있으면 count값만 return
    - findHello로 찾을 때 데이터가 있으면 Hello 오브젝트, 없으면 null
    - count 증가 시 name이 없으면, 데이터를 생성하고 1로 초기화, 있으면 count +1

---
+++ src/main/java/mjspring/springboot/HelloRepository.java
@@ -0,0 +1,12 @@
+package mjspring.springboot;
+
+public interface HelloRepository {
+    Hello findHello(String name);
+
+    void increaseCount(String name);
+
+    default int countOf(String name) {
+        Hello hello = findHello(name);
+        return hello == null ? 0 : hello.getCount();
+    }
+}
