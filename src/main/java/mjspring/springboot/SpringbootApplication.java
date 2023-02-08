c4df8cf95c34962aa1257adbdfda2890ab281f9c
Mu-jun <suo24691@gmail.com>
2023-02-08 11:54:31 +0900

    ConditionEvaluationReport을 이용하여 원하는 정보만 보기

--- src/main/java/mjspring/springboot/SpringbootApplication.java
+++ src/main/java/mjspring/springboot/SpringbootApplication.java
@@ -1,7 +1,10 @@
 package mjspring.springboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.PostConstruct;
@@ -23,5 +26,21 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
-
+	// 자동 구성 확인
+	@Bean
+	ApplicationRunner run(ConditionEvaluationReport report) {
+		return args -> {
+			System.out.println(report.getConditionAndOutcomesBySource().entrySet().stream()
+					.filter(co -> co.getValue().isFullMatch())	// 성공한 것만
+					.filter(co -> co.getKey().indexOf("Jmx") < 0) // Jmx빼고 검색
+					.map(co->{
+						System.out.println(co.getKey());
+						co.getValue().forEach(c -> {
+							System.out.println("\t" + c.getOutcome()); // 어떤 조건을 통과했는가를 알 수 있음.
+						});
+						System.out.println();
+						return co;
+					}).count());
+		};
+	}
 }
